91d478ec98b7b3f802d4ba90cbd90bad0cf28e1f
rylanharper <rylanharper@gmail.com>
2025-01-27 10:42:02 -0800

    feat: update vite config resolve @ path alias

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,15 +1,19 @@
 import { defineCliConfig } from 'sanity/cli';
+import path from 'path';
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: process.env.SANITY_STUDIO_DATASET
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID || '',
+    dataset: process.env.SANITY_STUDIO_DATASET || ''
   },
-  vite: {
-    resolve: {
-      alias: {
-        '@': __dirname
-      }
-    }
+  vite: (config) => {
+    if (!config.resolve) config.resolve = {};
+
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      '@': path.resolve(__dirname)
+    };
+
+    return config;
   }
 });
